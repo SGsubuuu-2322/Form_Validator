00cc216a32bc66bcda3e00b7de1c7c872b565ccf
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-04 23:27:17 +0530

    Added basic scripts for the script file for capturing the relevant HTML elements for further DOM manipulations...

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const cnfrmPassword = document.getElementById("password2");
+
+
