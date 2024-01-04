4ae245686371129c4da8ec85f02e0a8c96cd679a
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-04 23:36:21 +0530

    Added the scripts for username validation...

--- script.js
+++ script.js
@@ -4,4 +4,24 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const cnfrmPassword = document.getElementById("password2");
 
+function showErrorMsg(input, msg) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = msg;
+}
 
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (username.value === "") {
+    showErrorMsg(username, "Username is required....");
+  } else {
+    showSuccess(username);
+  }
+});
